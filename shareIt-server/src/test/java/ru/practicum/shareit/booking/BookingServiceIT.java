963c5d3795726727317fea8af0a96611cb3be22a
Tarasov Maxim <tarasovmaxxx@yandex.ru>
2025-10-15 21:40:36 +0700

    Спринт 16

--- shareIt-server/src/test/java/ru/practicum/shareit/booking/BookingServiceIT.java
+++ shareIt-server/src/test/java/ru/practicum/shareit/booking/BookingServiceIT.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingResponse;
@@ -20,6 +21,7 @@ import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 class BookingServiceIT {
 
     @Autowired
